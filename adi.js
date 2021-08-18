6b7eab38e7773e58e9800a305507e17a58e8270c
AdithyanBabu <80539335+AdithyanBabu@users.noreply.github.com>
2021-08-18 17:34:21 +0530

    Add files via upload

---
+++ adi.js
@@ -0,0 +1,16 @@
+function white()
+{
+    document.body.style.backgroundColor="white";
+}
+function red()
+{
+    document.body.style.backgroundColor="red";
+}
+function black()
+{
+    document.body.style.backgroundColor="black";
+}
+function khaki()
+{
+    document.body.style.backgroundColor="khaki";
+}
